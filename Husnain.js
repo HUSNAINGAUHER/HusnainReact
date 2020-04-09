908e334f54c94d02a826868f08773d0556c0679b
HUSNAINGAUHER <52236853+HUSNAINGAUHER@users.noreply.github.com>
2020-04-09 23:48:44 +0500

    first commit

---
+++ Husnain.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+function Husnain() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h1>Husnain Gauher</h1>
+
+        <img src="https://i.pinimg.com/originals/76/16/88/76168804bef97c698a15b50ce337342f.jpg"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Husnain;
